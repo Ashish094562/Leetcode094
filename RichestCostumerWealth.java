785b16b00d21c907b87473f6e4b26dade7157b44
ashish singh <ashishsingh094562@gmail.com>
2025-05-16 17:51:43 +0530

    CostumerWealth

---
+++ RichestCostumerWealth.java
@@ -0,0 +1,20 @@
+package com.ashish;
+
+public class RichestCostumerWealth {
+    public static void main(String[] args) {
+
+    }
+    public static int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for (int[] account : accounts) {
+            int sum = 0;
+            for (int i : account) {
+                sum += i;
+            }
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
