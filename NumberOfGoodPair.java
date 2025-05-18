b1952da56eb45ffc744a3c513966efdbafa2f26e
ashish singh <ashishsingh094562@gmail.com>
2025-05-18 11:04:46 +0530

    1

---
+++ NumberOfGoodPair.java
@@ -0,0 +1,25 @@
+package com.ashish;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NumberOfGoodPair {
+    public static void main(String[] args) {
+
+    }
+    static int numIdenticalPairs(int[] nums) {
+        Map<Integer,Integer> freq= new HashMap<>();
+        int count=0;
+        for(int num:nums){
+
+            if(freq.containsKey(num)){
+                count+=freq.get(num);
+                freq.put(num,freq.get(num)+1);
+            }
+            else{
+                freq.put(num,1);
+            }
+        }
+        return count;
+    }
+}
