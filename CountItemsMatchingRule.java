05adfbae90f936173017e778a5cf4bfc9ac2d75a
ashish singh <ashishsingh094562@gmail.com>
2025-05-18 11:49:01 +0530

    5

--- CountItemsMatchingRule.java
+++ CountItemsMatchingRule.java
@@ -1,4 +1,41 @@
 package com.ashish;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CountItemsMatchingRule {
+    public static void main(String[] args) {
+        // Create a List of List<String>
+        List<List<String>> items = new ArrayList<>();
+        String[][] arrayItems = {
+                {"phone", "blue", "pixel"},
+                {"computer", "silver", "lenovo"},
+                {"phone", "gold", "iphone"}
+        };
+
+        for (String[] item : arrayItems) {
+            items.add(Arrays.asList(item));
+        }
+
+        String ruleKey = "color", ruleValue = "silver";
+        int count = countMatches(items, ruleKey, ruleValue);
+        System.out.println(count); // Expected output: 1
+    }
+
+    static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = switch (ruleKey) {
+            case "color" -> 1;
+            case "name" -> 2;
+            default -> 0; // "type"
+        };
+
+        int count = 0;
+        for (List<String> item : items) {
+            if (item.get(index).equals(ruleValue)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
