314050a2d1a5bad0936e446588fa07ea1cffd88a
ashish singh <ashishsingh094562@gmail.com>
2025-05-19 07:47:54 +0530

    leetcode

--- CheckIfPangram.java
+++ CheckIfPangram.java
@@ -1,4 +1,24 @@
 package com.ashish;
 
 public class CheckIfPangram {
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        boolean ans = checkIfPangram(sentence);
+        System.out.println(ans);
+
+    }
+    static boolean checkIfPangram(String sentence) {
+        boolean[] seen = new boolean[26];
+        for(char c:sentence.toCharArray()){
+            if(Character.isAlphabetic(c)){
+                seen[Character.toLowerCase(c) - 'a'] = true;
+            }
+        }
+        for (boolean letter : seen) {
+            if (!letter) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- PeakIndexInaMountainArray.java
+++ PeakIndexInaMountainArray.java
@@ -1,4 +1,24 @@
 package com.ashish;
 
 public class PeakIndexInaMountainArray {
+    public static void main(String[] args) {
+        int[] arr ={0,2,1,0};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
+    static int peakIndexInMountainArray(int[] arr) {
+        int start =0;
+        int end = arr.length-1;
+        while(start <end){
+            int mid = start + (end-start)/2;
+            if (arr[mid] > arr[mid+1]) {
+                //desc part of array
+                end = mid;
+            }
+            else{
+                // asc part of array
+                start= mid+1;
+            }
+        } return start;
+    }
 }
