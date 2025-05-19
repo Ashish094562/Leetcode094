965b2f0a24c409bc805b6e44d8b7c1293d55379f
ashish singh <ashishsingh094562@gmail.com>
2025-05-19 23:45:59 +0530

    leetcode

---
+++ GuessNumberhigherOrLower.java
@@ -0,0 +1,28 @@
+package com.ashish;
+
+public class GuessNumberhigherOrLower {
+    public static void main(String[] args) {
+        int[] arr={2,4,6,7,8,9,11,13,15,17};
+        int n=10,pick=6;
+
+    }
+    static int guessNumber(int[] arr,int n) {
+        int start=0,end =n;
+
+        while (start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+            int res= guess(mid);
+            if(res==0){
+                return mid;
+            }
+            else if (res<0) {
+                end = mid - 1;
+            } else  {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
