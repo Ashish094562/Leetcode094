37b7f3ad018d992f79392c0958a1451a653adbe2
ashish singh <ashishsingh094562@gmail.com>
2025-05-20 08:57:49 +0530

    leetcode

---
+++ KthMissingPositiveNumber.java
@@ -0,0 +1,21 @@
+package com.ashish;
+
+public class KthMissingPositiveNumber {
+    public static void main(String[] args) {
+        int[] arr={2,3,4,7,11};
+        int k=5;
+        System.out.println(findKthPositive(arr,k));
+    }
+    static int findKthPositive(int[] arr, int k) {
+        int start=0,end=arr.length-1;
+        while(start<=end){
+            int mid = start +(end-start)/2;
+            int missing = arr[mid]- (mid+1);
+            if(missing<k)
+                start = mid+1;
+            else
+                end = mid-1;
+
+        }return start+k;
+    }
+}
