b70e1b72ef02b6baabbd2f00cbef5a71574c6cf0
ashish singh <ashishsingh094562@gmail.com>
2025-07-02 08:55:55 +0530

    709

---
+++ ToLowerCase.java
@@ -0,0 +1,11 @@
+package com.ashish;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        String s = "LOVELY";
+        System.out.println(toLowerCase(s));
+    }
+    static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
