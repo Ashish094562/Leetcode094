10782ba41147d66388883a823bfb50334e0cfcb5
ashish singh <ashishsingh094562@gmail.com>
2025-07-26 12:28:10 +0530

    37

---
+++ SudokuSolverLeet.java
@@ -0,0 +1,63 @@
+package com.ashish;
+
+class SudokuSolverLeet {
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+        System.out.println(solve(board));
+
+    }
+    static boolean solve(char[][] board) {
+        int n = board.length;
+
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == '.') {
+                    for (char num = '1'; num <= '9'; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = '.'; // backtrack
+                            }
+                        }
+                    }
+                    return false; // no valid number found, trigger backtrack
+                }
+            }
+        }
+
+        return true; // all cells filled
+    }
+    // checking all the conditions 
+    static boolean isSafe(char[][] board, int row, int col, char num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        int rowStart = row - row % 3;
+        int colStart = col - col % 3;
+
+        for (int r = rowStart; r < rowStart + 3; r++) {
+            for (int c = colStart; c < colStart + 3; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
